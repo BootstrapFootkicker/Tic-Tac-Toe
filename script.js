9745ecd35b0e8f5dee2d5b4a4d071c7ddce25af7
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2022-12-29 00:31:42 -0500

    Horizontal Win Added check in game logic for horizontal wins

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const Gameboard = (() => {
 
     const getCellList = () => cells;
     return {
-       getCellList,
+        getCellList,
 
     };
 })();
@@ -20,12 +20,22 @@ const GameLogic = (() => {
     const isEmpty = (cell) => {
         return cell.textContent === ''
     }
+    const isHorizontalWin = (nodelist) => {
+        if (nodelist[0].textContent === nodelist[1].textContent &&
+            nodelist[1].textContent === nodelist[2].textContent && nodelist[0].textContent !== '') {
+            return true;
+        } else if (nodelist[3].textContent === nodelist[4].textContent &&
+            nodelist[4].textContent === nodelist[5].textContent && nodelist[3].textContent !== '') {
+            return true;
+        } else return nodelist[6].textContent === nodelist[7].textContent &&
+            nodelist[7].textContent === nodelist[8].textContent && nodelist[6].textContent !== '';
+    }
 
     const isXTurn = () => {
         return turn % 2 === 1;
     }
     return {
-        getTurn, isXTurn, incrementTurn, isEmpty,
+        getTurn, isXTurn, incrementTurn, isEmpty, isHorizontalWin
 
     };
 })();
@@ -44,15 +54,18 @@ const Player = (name, playerSign) => {
 let cellNodeList = Gameboard.getCellList();
 cellNodeList.forEach(cell => cell.addEventListener('click', () => {
 
-    if (GameLogic.isXTurn()&& GameLogic.isEmpty(cell)) {
+    if (GameLogic.isXTurn() && GameLogic.isEmpty(cell)) {
         cell.textContent = 'X';
         GameLogic.incrementTurn();
-    } else if (!GameLogic.isXTurn() && GameLogic.isEmpty(cell)){
+    } else if (!GameLogic.isXTurn() && GameLogic.isEmpty(cell)) {
         cell.textContent = "O";
         GameLogic.incrementTurn();
-    }
-    else {
+    } else {
         alert("No Way! Spot Already Taken!")
     }
+    if (GameLogic.isHorizontalWin(Gameboard.getCellList())) {
+        console.log(("horizontal win!"))
+        alert("horizontal win")
+    }
 }))
-console.log(cellNodeList);
+console.log(Gameboard.getCellList());
